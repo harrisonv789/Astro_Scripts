5bc878b528b5c2c5b34984789946adbb5a42a1b7
Harrison Verrios <harrison.v1@live.com>
2021-09-09 09:21:11 +1000

    Added subdirectory to the scattered script

--- scattered.py
+++ scattered.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# Import all relevant modules
 from matplotlib.patches import Circle
 from modules.casa_cube import casa_cube as casa
 import matplotlib.pyplot as plt
@@ -16,8 +17,8 @@ from modules.params.get_param import Params
 params = Params(len(sys.argv) > 1 and sys.argv[1].lower() == "-defaults")
 
 # Directory paths
-directory = params.ask("Directory", "../Output/Scattered/")
-filename = directory + params.ask("Filename", "scattered")
+root_directory = params.ask("Root Directory", "../Output/Scattered/New/")
+sub_directory = params.ask("Folder", "Gas")
 
 # Set the type (Qphi or Uphi)
 image_type = params.ask("Type", "PI", ["PI", "Qphi", "Uphi"])
@@ -35,6 +36,9 @@ image_vmax = params.ask("Max Flux", 1e-18)
 # Add stars or not
 image_plotstars = params.ask("Plot Stars", True, [True, False])
 
+# Get the plot name
+filename = root_directory + sub_directory + params.ask("Filename", "scattered.pdf")
+
 # Add a new line
 print("\n---\n")
 
@@ -51,14 +55,14 @@ import pymcfost as mcfost
 fig, axes = plt.subplots()
 
 # Get the MCFOST data
-mod_cont = mcfost.Image(directory)
+mod_cont = mcfost.Image(root_directory + sub_directory)
 
 # Create the image
 image = mod_cont.plot(
     ax = axes,
     colorbar = True,
     scale = image_scale,
-    plot_stars = True,
+    plot_stars = image_plotstars,
     type = image_type,
     psf_FWHM=image_FWHM,
     vmin = image_vmin,
