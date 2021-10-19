c747752989a15b9f55719be3fb56a848f087e993
Harrison Verrios <harrison.v1@live.com>
2021-10-19 17:50:29 +1100

    Added options for multiple scattered light images

--- scattered.py
+++ scattered.py
@@ -18,11 +18,11 @@ params = Params()
 
 # Directory paths
 root_directory = params.get("root")
-sub_directory = str(params.get("dir"))
+inputs = params.get_array("inputs", strings = True)
 
 # Get whether to use CASA cube or not
 use_casa = params.get("casa")
-if use_casa: sub_directory = params.get("dir_casa")
+if use_casa: inputs = [params.get("dir_casa")]
 
 # Set the type (Qphi or Uphi)
 image_type = params.get("type")
@@ -37,12 +37,20 @@ image_FWHM = params.get("fwhm")
 image_vmin = params.get("fmin")
 image_vmax = params.get("fmax")
 
+# The limits of the graph
+# (max_x, min_x, min_y, max_y)
+limits_max = params.get("limits")
+limits = [limits_max, -limits_max, -limits_max, limits_max]
+
 # Map the colours to matplotlib colours
 colors = {"grey": "Greys_r", "inferno": "inferno", "viridis": "viridis", "normal": "inferno", "sepia": "copper"}
 cmap = colors[params.get("cmap")]
 
 # Add stars or not
 image_plotstars = params.get("stars")
+
+# Python Figure Size
+image_size = 4.0
 
 # Whether to show the scattered light image to the screen or not
 image_show = params.get("show")
@@ -51,7 +59,7 @@ image_show = params.get("show")
 image_save= params.get("save")
 
 # Get the plot name
-filename = root_directory + sub_directory + params.get("file")
+filename = root_directory + params.get("file")
 
 
 
@@ -64,18 +72,24 @@ import pymcfost as mcfost
 from modules.casa_cube import casa_cube as casa
 
 # Create the axes
-fig, axes = plt.subplots()
+fig, axes = plt.subplots(
+    nrows = 1,
+    ncols = len(inputs),
+    figsize = (image_size * len(inputs), image_size),
+    sharex='all',
+    sharey='all'
+)
 
 
 # If using CASA
 if use_casa:
 
     # Get the CASA daa
-    cont =  casa.Cube(root_directory + sub_directory + "/RT.fits")
+    cont =  casa.Cube(root_directory + inputs[0] + "/RT.fits")
 
     # Creae the image
     image = cont.plot(
-        ax = axes,
+        ax = axes[0,0],
         colorbar = True,
         color_scale = image_scale,
         fmin = image_vmin * 1e16,
@@ -87,22 +101,27 @@ if use_casa:
 # Otherwise, if using PyMCFOST
 else:
 
-    # Get the MCFOST data
-    mod_cont = mcfost.Image(root_directory + sub_directory)
+    # Loop through each of the files
+    for idx, data in enumerate(inputs):
 
-    # Create the image
-    image = mod_cont.plot(
-        title = "Scattered Light Image - %s $M_\mathrm{jup}$" % sub_directory,
-        ax = axes,
-        colorbar = True,
-        scale = image_scale,
-        plot_stars = image_plotstars,
-        type = image_type,
-        psf_FWHM=image_FWHM,
-        vmin = image_vmin,
-        vmax = image_vmax,
-        cmap = cmap
-    )
+        # Get the MCFOST data
+        mod_cont = mcfost.Image(root_directory + data)
+
+        # Create the image
+        image = mod_cont.plot(
+            title = "%s $M_\mathrm{jup}$" % data,
+            ax = axes[idx],
+            colorbar = idx == len(inputs) - 1,
+            scale = image_scale,
+            no_ylabel = idx > 0,
+            limits = limits,
+            plot_stars = image_plotstars,
+            type = image_type,
+            psf_FWHM=image_FWHM,
+            vmin = image_vmin,
+            vmax = image_vmax,
+            cmap = cmap
+        )
 
 
 #------------------------------#
